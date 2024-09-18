aa33a1d0c06bcd25e4f29471b82f3fff724f77f1
Govinda Tudu <govindat356@gmail.com>
2024-09-18 10:37:37 +0530

    successfull

---
+++ script.js
@@ -0,0 +1,71 @@
+//for each✅
+// let arr=[1,2,3,4];
+// arr.forEach(function(val){
+//     console.log(val+"hello");
+    
+// }) 
+
+
+//map✅  
+// let arr=[1,2,3,4] 
+// // i want a new array which return some function in old array 
+// let ans=arr.map(function(val){
+//     return 13;
+// })
+// console.log(ans);
+
+
+//filter ✅ 
+// let arr=[1,2,3,4]; 
+// let finalVal=arr.filter(function(val){
+//     if(val>3)
+//     {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// })
+// console.log(finalVal);
+
+
+//find ✅ 
+// let arr=[1,2,3,4,4,5,6];
+// let result=arr.find(function(val){
+//     if(val==4){
+//         return val;  //filter only takes the true/false value ,we cant pass some value in it
+//     }
+// })
+// console.log(result);
+
+
+//indexOf✅ 
+// let arr=[1,2,3,4,5];
+// let result=arr.indexOf(3)
+// console.log(result);
+
+
+ //objects✅ 
+//  let obj={
+//     name:"govinda"
+//  }
+//  Object.freeze(obj);
+//  obj.name="tudu"
+//  console.log(obj.name);
+
+//return ✅ 
+// function abcd()
+// {
+//     return 12;
+// }
+// let result=abcd();
+// console.log(result);
+
+
+// async js coding✅ 
+ //line by line code chale synchronous
+ //jo bhi code asychro nature ka ho use side stack me bhej do aur agle code chalao jo bhi sync nature ka ho ,job bhi sara sync //code chal jaaa tab check karo async code complete hua ya nahin,agar complete huo ho to main stack main lejaooo
+
+
+ 
+ 
